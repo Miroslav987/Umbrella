9e5c377b16680e1bdbb90584db3cc5ebe8af9e11
Miros <mirsichkar@gmail.com>
2024-09-17 21:43:21 +0600

    new_style

--- src/Components/CardLoad.tsx
+++ src/Components/CardLoad.tsx
@@ -1,21 +1,176 @@
-
+import "../styles/CardLoad.scss"
 
 
 export const CardLoad =()=>{
 
     return(
         <>
-        <article
-
-          className="card">
-{/*
-               <img src={data.img1} alt="" /> */}
+        <article className="card_load">
+                <div className="img"></div>
                 <article className="card_info">
                     <p className="text_name"></p>
                     <p className="text_description"></p>
+                    <p className="text_description"></p>
+                    <p className="text_description"></p>
+
                 </article>
           <button ><p className="text_price">сом</p></button>
           </article>
+        <article className="card_load">
+                <div className="img"></div>
+                <article className="card_info">
+                    <p className="text_name"></p>
+                    <p className="text_description"></p>
+                    <p className="text_description"></p>
+                    <p className="text_description"></p>
+
+                </article>
+          <button ><p className="text_price">сом</p></button>
+          </article>
+        <article className="card_load">
+                <div className="img"></div>
+                <article className="card_info">
+                    <p className="text_name"></p>
+                    <p className="text_description"></p>
+                    <p className="text_description"></p>
+                    <p className="text_description"></p>
+
+                </article>
+          <button ><p className="text_price">сом</p></button>
+          </article>
+        <article className="card_load">
+                <div className="img"></div>
+                <article className="card_info">
+                    <p className="text_name"></p>
+                    <p className="text_description"></p>
+                    <p className="text_description"></p>
+                    <p className="text_description"></p>
+
+                </article>
+          <button ><p className="text_price">сом</p></button>
+          </article>
+        <article className="card_load">
+                <div className="img"></div>
+                <article className="card_info">
+                    <p className="text_name"></p>
+                    <p className="text_description"></p>
+                    <p className="text_description"></p>
+                    <p className="text_description"></p>
+
+                </article>
+          <button ><p className="text_price">сом</p></button>
+          </article>
+        <article className="card_load">
+                <div className="img"></div>
+                <article className="card_info">
+                    <p className="text_name"></p>
+                    <p className="text_description"></p>
+                    <p className="text_description"></p>
+                    <p className="text_description"></p>
+
+                </article>
+          <button ><p className="text_price">сом</p></button>
+          </article>
+        <article className="card_load">
+                <div className="img"></div>
+                <article className="card_info">
+                    <p className="text_name"></p>
+                    <p className="text_description"></p>
+                    <p className="text_description"></p>
+                    <p className="text_description"></p>
+
+                </article>
+          <button ><p className="text_price">сом</p></button>
+          </article>
+        <article className="card_load">
+                <div className="img"></div>
+                <article className="card_info">
+                    <p className="text_name"></p>
+                    <p className="text_description"></p>
+                    <p className="text_description"></p>
+                    <p className="text_description"></p>
+
+                </article>
+          <button ><p className="text_price">сом</p></button>
+          </article>
+        <article className="card_load">
+                <div className="img"></div>
+                <article className="card_info">
+                    <p className="text_name"></p>
+                    <p className="text_description"></p>
+                    <p className="text_description"></p>
+                    <p className="text_description"></p>
+
+                </article>
+          <button ><p className="text_price">сом</p></button>
+          </article>
+        <article className="card_load">
+                <div className="img"></div>
+                <article className="card_info">
+                    <p className="text_name"></p>
+                    <p className="text_description"></p>
+                    <p className="text_description"></p>
+                    <p className="text_description"></p>
+
+                </article>
+          <button ><p className="text_price">сом</p></button>
+          </article>
+        <article className="card_load">
+                <div className="img"></div>
+                <article className="card_info">
+                    <p className="text_name"></p>
+                    <p className="text_description"></p>
+                    <p className="text_description"></p>
+                    <p className="text_description"></p>
+
+                </article>
+          <button ><p className="text_price">сом</p></button>
+          </article>
+        <article className="card_load">
+                <div className="img"></div>
+                <article className="card_info">
+                    <p className="text_name"></p>
+                    <p className="text_description"></p>
+                    <p className="text_description"></p>
+                    <p className="text_description"></p>
+
+                </article>
+          <button ><p className="text_price">сом</p></button>
+          </article>
+        <article className="card_load">
+                <div className="img"></div>
+                <article className="card_info">
+                    <p className="text_name"></p>
+                    <p className="text_description"></p>
+                    <p className="text_description"></p>
+                    <p className="text_description"></p>
+
+                </article>
+          <button ><p className="text_price">сом</p></button>
+          </article>
+        <article className="card_load">
+                <div className="img"></div>
+                <article className="card_info">
+                    <p className="text_name"></p>
+                    <p className="text_description"></p>
+                    <p className="text_description"></p>
+                    <p className="text_description"></p>
+
+                </article>
+          <button ><p className="text_price">сом</p></button>
+          </article>
+        <article className="card_load">
+                <div className="img"></div>
+                <article className="card_info">
+                    <p className="text_name"></p>
+                    <p className="text_description"></p>
+                    <p className="text_description"></p>
+                    <p className="text_description"></p>
+
+                </article>
+          <button ><p className="text_price">сом</p></button>
+          </article>
+          
         </>
     )
 }
